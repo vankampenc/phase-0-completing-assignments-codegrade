d2652427061de4c5b0c7123b26477ec4c92656d4
Christopher Van Kampen <christopheryungvankampen@gmail.com>
2024-08-20 20:16:57 -0400

    js added, created footer and skills

--- JS/index.js
+++ JS/index.js
@@ -1,13 +1,30 @@
-let today = new Date();
-let thisYear = today.getFullYear()
-let footer = 
-
 // Adding a footer with DOM
-const addFooter = document.createElement('footer');
-    // Footer Styling
-addFooter.textContent = 'footer content';
-addFooter.style.backgroundColor = 'red';
-addFooter.style.textAlign = 'center'
-    // appending
-document.body.appendChild(addFooter);
+ const footer = document.createElement('footer');
+ let body = document.querySelector('body');
+ // appending
+ body.appendChild(footer);
+ 
 
+let today = new Date();
+let thisYear = today.getFullYear();
+let myName = "Christopher Van Kampen"
+
+// Footer Styling
+footer.style.backgroundColor = 'red';
+footer.style.textAlign = 'center'
+
+//Copyright
+let copyright = document.createElement('p')
+copyright.innerHTML = `${myName} ${thisYear}`;
+footer.appendChild(copyright)
+
+// Skills List
+let skillsList = ["JavaScript", "HTML", "CSS", "GitHub", "Google Suite"];
+let skillsSection = document.getElementById('Skills');
+let skillsUL = skillsSection.querySelector('ul');
+
+for (let skill of skillsList) {
+    let skillLI = document.createElement('li');
+    skillLI.innerHTML = skill;
+    skillsUL.appendChild(skillLI);
+}
